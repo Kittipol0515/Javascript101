aa7a4ff5374e803b793afd49fa10149511fb990b
kittipol <kittipol.surapinchai@gmail.com>
2023-03-10 13:37:03 +0700

    practice javascript

--- JS101/script.js
+++ JS101/script.js
@@ -1,3 +1,15 @@
 console.log('Hello world')
 
-let myName ='Nomain'
+let myName ='Nomain';
+let lastName = 'Kitti';
+
+let fullName = myName + ' ' + lastName;
+console.log(fullName);
+
+console.log(`My code name is ${myName} and my name is ${fullName}`);
+
+let email = 'kittipol.surapinchai@gmail.com';
+// let findLetter = email.includes(z);
+// console.log(findLetter);
+
+
