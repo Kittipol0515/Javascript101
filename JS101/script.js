de11599ee252486cc8bbb702f3a819d7be810819
kittipol <kittipol.surapinchai@gmail.com>
2023-03-10 14:22:19 +0700

    Edit Javascript

--- JS101/script.js
+++ JS101/script.js
@@ -1,3 +1,5 @@
+// section 1
+
 console.log('Hello world')
 
 let myName ='Nomain';
@@ -12,4 +14,45 @@ let email = 'kittipol.surapinchai@gmail.com';
 // let findLetter = email.includes(z);
 // console.log(findLetter);
 
+let bidMas = 5 * (10-3)**2;
+console.log(bidMas);
+
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius,pi);
+
+// section2
+let selectedFruits = ['Apple','Banana','Orange','Dragon Fruit']
+selectedFruits[4] = 'Valorant';
+selectedFruits[5] = 5;
+selectedFruits[1] = 'soda';
+console.log(selectedFruits[1]);
+console.log(selectedFruits[4]);
+console.log(selectedFruits.length);
+
+let user = {
+    firstName :'Kitti',
+    lastName :'Nomain',
+    myCity : 'Tokyo',
+    myAge :'25',
+    location : 'Japan',
+    readingList:['HTML','CSS','JavaScript'],
+}
+
+console.log(user);
+console.log(user.myCity);
+user.myAge = 26;
+console.log(user.myAge)
+console.log(user.readingList[0]);
+
+
+// coding Pratice
+let x = 'Blue';
+x = 8;
+console.log(x);
+console.log(typeof x);
+
+x+= 4;
+console.log(x);
 
