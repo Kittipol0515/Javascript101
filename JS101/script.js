7e2f7555d8c69aae370adb3f1f830655570e1532
kittipol <kittipol.surapinchai@gmail.com>
2023-03-14 13:18:16 +0700

    edit js

--- JS101/script.js
+++ JS101/script.js
@@ -123,4 +123,20 @@ function aboutme(firstName, location, hobby) {
   }
 aboutme("Kitti", "Bangkok", "playing games"); 
 
+// function
+function calWithReturn (num1,num2){
+    return num1 + num2; //allow to install somewhere
+  }
+  const total = calWithReturn(5,9);
+  console.log(total);
+  //then total can use later
+  function calUseReturn(total){
+      console.log (total*5);
+  }
+  calUseReturn(total);
+  function cal(a){
+      console.log (a+1);
+  }
+  cal(total);
+
 
