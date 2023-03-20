676ff1e20963fc83ad39001e6c408ea84a67f844
kittipol <kittipol.surapinchai@gmail.com>
2023-03-20 12:01:21 +0700

    edit note js

--- JS101/object.js
+++ JS101/object.js
@@ -21,3 +21,15 @@ console.log(favGame['Good game']);
 favGame.ingame();
 
 // for in object , for or array of
+function Person(first,last,nick,age,school,eye) {
+    this.fisrtName = first;
+    this.lastname = last;
+    this.nick = nick;
+    this.age = age;
+    this.school = school;
+    this.eye = eye;
+}
+
+const person =new Person('Kittipol','Nomian','Kitti','24' , 'IIS-RU' , 'Red' );
+
+console.log(person);
