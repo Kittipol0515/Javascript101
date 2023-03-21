582ec651f3fe68f77ea09280fbae09d478024555
kittipol <kittipol.surapinchai@gmail.com>
2023-03-21 11:44:51 +0700

    edit js

---
+++ JS101/numbers.js
@@ -0,0 +1,13 @@
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+console.log(equal1());
