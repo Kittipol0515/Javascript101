ccfcee7c30c345394dbd1533bd1bac9e12a91d1a
kittipol <kittipol.surapinchai@gmail.com>
2023-03-23 13:56:29 +0700

    edit note js

---
+++ JS101/Promise/Promise.js
@@ -0,0 +1,17 @@
+// const promise = new Promise((resolve, reject) => {
+//     let sum = 1+2;
+
+//     if (sum == 3) {
+//         resolve('Success');
+//     } else {
+//         reject('Error');
+//     }
+// });
+
+// promise.then((message) => {
+//     console.log('This is success resolve' + message);
+// }). catch((message) => {
+//     console.log('This is error resolve' + message);
+// })
+
+
