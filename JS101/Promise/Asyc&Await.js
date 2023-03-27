e99ad938704b847393da1771d77cff15437522c4
kittipol <kittipol.surapinchai@gmail.com>
2023-03-27 16:29:22 +0700

    edit note js

--- JS101/Promise/Asyc&Await.js
+++ JS101/Promise/Asyc&Await.js
@@ -20,4 +20,74 @@ const run = async () => {
     
 };
 
-run();
+run();  
+
+
+// async function kitchen (){ //it's recommended to use a try/catch block to catch any errors that might occur during the asynchronous operations
+//     try {
+    
+//       await time (10000);
+//       console.log(`${stocks.coffee[0]}`)  
+      
+//       await time (0000);
+//       console.log("started making coffee");
+      
+//       await time (2000);
+//       console.log('coffee bean in process');
+//       await time (2000);
+//       console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were selected`);
+
+//       await time (1000);
+//       console.log('coffee mixed');
+
+//       await time (2000);
+//       console.log(`${stocks.cup[0]}`);
+
+//       await time (2000);
+//       console.log(`${stocks.topping[0]} was selected`);
+
+//       await time (2000);
+//       console.log('coffee was served');
+
+//     }
+//     catch(error){
+//         console.log("customer left");
+//     }
+//     finally{
+//         console.log("day ended, shop is closed");
+//     }
+// }
+// kitchen();
+// console.log('Queue outsite');
+// console.log('staff cleaning the table');
+// Collapse
+
+
+
+// fetch('https://reqres.in/api/users/2')
+// .then((response) => response.json())
+// .then((data) => {
+//   console.log('Success:', data);
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
